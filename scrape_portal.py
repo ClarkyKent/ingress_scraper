27fe7ef6428afdf2ec811d7953cec13a1b538b6d
Ashu <ClarkyKent@users.noreply.github.com>
2019-10-14 15:08:23 +0200

    boundary box added to get info of portals from a geofence. This will get all portals in given BBOX geofence(usually rectangle) and update DB for missing names and url. Populating ingress portal in manual DB is possible but need a query for that

--- scrape_portal.py
+++ scrape_portal.py
@@ -2,6 +2,7 @@ from ingressAPI import IntelMap
 import argparse
 import json
 from pymysql import connect
+import mercantile
 
 # Python2 and Python3 compatibility
 try:
@@ -80,7 +81,9 @@ def create_config(config_path):
     config['encoding'] = config_raw.get(
         'Other',
         'ENCODING')
-
+    config['bbox'] = config_raw.get(
+        'Area',
+        'BBOX')
 
     return config
 
@@ -100,15 +103,44 @@ def print_configs(config):
     print("")
     print("~"*15)
 
+def get_all_portals(login, bbox):
+    tiles = list(mercantile.tiles(*bbox))
+    print("Number of tiles in boundry are : ",len(tiles))
+    timed_out_items = []
+    portals = []
+    portal_id = []
+    for tile in tiles:
+        iitc_xtile = int( tile[0] - 396 )        # To be check
+        iitc_ytile = int( tile[1] - 276 )    # To be check
+        
+        iitc_tile_name  = ('{0}_{1}_{2}_0_8_100').format(zoom, iitc_xtile, iitc_ytile)
+        print("Getting portals from tile : ", iitc_tile_name)
+        tiles_data = login.get_entities([iitc_tile_name])
+
+        if 'result' in tiles_data:
+                for data in tiles_data['result']['map']:
+                    if 'error' in tiles_data['result']['map'][data]:
+                        timed_out_items.append(data)
+                    else:
+                        for entry in tiles_data['result']['map'][data]['gameEntities']:
+                            #print(entry)
+                            if entry[2][0] == 'p':
+                                portal_id.append(entry[0])
+                                portals.append(entry[2])
+                                #print(entry[0])
+    return portals, portal_id
 
 if __name__ == "__main__":
     portal_name = 8
     portal_url = 7
+    zoom = 15
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-p", "--pokestop", action='store_true', help="updates pokestop only")
     parser.add_argument(
         "-g", "--gym", action='store_true', help="updates gyms only")
+    parser.add_argument(
+        "-all", "--all_poi", action='store_true', help="updates gyms, pokestops and ingress portals")
     parser.add_argument(
         "-c", "--config", default="default.ini", help="Config file to use")
     args = parser.parse_args()
@@ -128,12 +160,15 @@ if __name__ == "__main__":
 
     mycursor_r = mydb_r.cursor()
     print("Connection clear")
-    
+    updated_gyms = 0
+    updated_pokestops = 0
     
     IngressLogin = IntelMap(config['cookies'], config['username'], config['pwd'])
-    
-    if args.gym:
-        
+
+    if args.all_poi:
+        bbox = list(map(float, config['bbox'].split(',')))
+        bbox.append(zoom)
+        all_portal_details, all_portals_id = get_all_portals(IngressLogin, bbox)
         gym_sel_query = GYM_SELECT_QUERY.format(
                     db_gym_id=config['db_gym_id'],
                     db_name=config['db_r_name'],
@@ -152,22 +187,24 @@ if __name__ == "__main__":
             )
             
         for gym_id in gym_result_ids:
-            ingress_portal_details = IngressLogin.get_portal_details(gym_id[0])
-            if ingress_portal_details is not None:
-                insert_args = (ingress_portal_details.get('result')[portal_name],  ingress_portal_details.get('result')[portal_url],  gym_id[0] )
+            try:
+                single_portal_detail = all_portal_details[all_portals_id.index(gym_id[0])]
+                insert_args = (single_portal_detail[portal_name],  single_portal_detail[portal_url],  gym_id[0] )
                 try:
                     mycursor_r.execute(gym_update_query, insert_args)
-                    print(gym_id[0], ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url], ' succeded and updated in DB')
+                    updated_gyms = updated_gyms +1
+                    print(gym_id[0], single_portal_detail[portal_name], single_portal_detail[portal_url], ' succeeded and updated in DB')
                 except Exception as e:
                     print("~"*15)
                     print(gym_id[0], ' Could not update in DB')
                     print("~"*15)
                     print(e)
                     print("~"*15)
-            else:
-                print('Could not parse portal info for ', gym_id[0], 'check if it is valid portal', )
-            
-    if args.pokestop:
+            except Exception as e:
+                print(e)
+                print('Did not {0} find missing gym_id in given BBOX boundry').format(gym_id)
+        print('Total gyms updated: ', updated_gyms)
+
     
         pokestop_sel_query = POKESTOP_SELECT_QUERY.format(
                     db_pokestop_id=config['db_pokestop_id'],
@@ -187,13 +224,13 @@ if __name__ == "__main__":
                 )
                 
         for stop_id in pokestop_result_ids:
-            ingress_portal_details = IngressLogin.get_portal_details(stop_id[0])
-            if ingress_portal_details is not None:
-                print(ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url])
-                insert_args = (ingress_portal_details.get('result')[portal_name],  ingress_portal_details.get('result')[portal_url],  stop_id[0] )                
+            try:
+                single_portal_detail = all_portal_details[all_portals_id.index(stop_id[0])]
+                insert_args = (single_portal_detail[portal_name],  single_portal_detail[portal_url],  stop_id[0] )                
                 try:
                     mycursor_r.execute(pokestop_update_query, insert_args)
-                    print(stop_id[0], ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url], ' succeded and updated in DB')
+                    updated_pokestops = updated_pokestops + 1
+                    print(stop_id[0], single_portal_detail[portal_name], single_portal_detail[portal_url], ' succeeded and updated in DB')
                 except Exception as e:
                     print("~"*15)
                     print(stop_id[0], ' Could not update in DB')
@@ -201,5 +238,89 @@ if __name__ == "__main__":
                     print(e)
                     print("~"*15)
                     
-            else:
-                print('Could not parse portal info for ', stop_id[0], 'check if it is valid portal', )
+            except Exception as e:
+                print('Did not find missing pokestop_id in given BBOX boundry' )
+        print('Total pokestops updated: ', updated_pokestops)
+
+    else:
+        if args.gym:
+        
+            gym_sel_query = GYM_SELECT_QUERY.format(
+                        db_gym_id=config['db_gym_id'],
+                        db_name=config['db_r_name'],
+                        db_gym=config['db_gym'],
+                        db_gym_name=config['db_gym_name']
+                    )
+            mycursor_r.execute(gym_sel_query)
+            gym_result_ids = mycursor_r.fetchall()
+            
+            print('Total gyms found: ', len(gym_result_ids))
+            
+            gym_update_query = GYM_UPDATE_QUERY.format(
+                    db_name=config['db_r_name'],
+                    db_gym=config['db_gym'],
+                    db_gym_name=config['db_gym_name'],
+                    db_gym_image=config['db_gym_image'],
+                    db_gym_id=config['db_gym_id'],
+                )
+                
+            for gym_id in gym_result_ids:
+                ingress_portal_details = IngressLogin.get_portal_details(gym_id[0])
+                if ingress_portal_details is not None:
+                    insert_args = (ingress_portal_details.get('result')[portal_name],  ingress_portal_details.get('result')[portal_url],  gym_id[0] )
+                    try:
+                        mycursor_r.execute(gym_update_query, insert_args)
+                        updated_gyms = updated_gyms +1
+                        print(gym_id[0], ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url], ' succeeded and updated in DB')
+                    except Exception as e:
+                        print("~"*15)
+                        print(gym_id[0], ' Could not update in DB')
+                        print("~"*15)
+                        print(e)
+                        print("~"*15)
+                else:
+                    print('Could not parse portal info for ', gym_id[0], 'check if it is valid portal', )
+                    
+            print('Total gyms updated: ', updated_gyms)
+            
+        if args.pokestop:
+        
+            pokestop_sel_query = POKESTOP_SELECT_QUERY.format(
+                        db_pokestop_id=config['db_pokestop_id'],
+                        db_name=config['db_r_name'],
+                        db_pokestop=config['db_pokestop'],
+                        db_pokestop_name=config['db_pokestop_name']
+                    )
+            mycursor_r.execute(pokestop_sel_query)
+            pokestop_result_ids = mycursor_r.fetchall()
+            
+            print('Total pokestops found: ', len(pokestop_result_ids))
+            
+            pokestop_update_query = POKESTOP_UPDATE_QUERY.format(
+                        db_name=config['db_r_name'],
+                        db_pokestop=config['db_pokestop'],
+                        db_pokestop_name=config['db_pokestop_name'],
+                        db_pokestop_image=config['db_pokestop_image'],
+                        db_pokestop_id=config['db_pokestop_id'],
+                    )
+                    
+            for stop_id in pokestop_result_ids:
+                ingress_portal_details = IngressLogin.get_portal_details(stop_id[0])
+                if ingress_portal_details is not None:
+                    print(ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url])
+                    insert_args = (ingress_portal_details.get('result')[portal_name],  ingress_portal_details.get('result')[portal_url],  stop_id[0] )                
+                    try:
+                        mycursor_r.execute(pokestop_update_query, insert_args)
+                        print(stop_id[0], ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url], ' succeeded and updated in DB')
+                        updated_pokestops = updated_pokestops + 1
+                    except Exception as e:
+                        print("~"*15)
+                        print(stop_id[0], ' Could not update in DB')
+                        print("~"*15)
+                        print(e)
+                        print("~"*15)
+                        
+                else:
+                    print('Could not parse portal info for ', stop_id[0], 'check if it is valid portal', )
+                    
+            print('Total pokestops updated: ', updated_pokestops)
