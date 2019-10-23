9aea015e8c8f6a6c875aa61eeab5e8b0b2508f40
Ashu <ClarkyKent@users.noreply.github.com>
2019-10-23 10:35:24 +0200

    refactored and put portal name when not updated in error msg

--- scrape_portal.py
+++ scrape_portal.py
@@ -22,7 +22,7 @@ GYM_UPDATE_QUERY = """UPDATE {db_name}.{db_gym} set {db_gym_name}= %s, {db_gym_i
 POKESTOP_SELECT_QUERY = """SELECT {db_pokestop_id} FROM {db_name}.{db_pokestop} WHERE {db_pokestop_name} is NULL AND {db_pokestop_id} like '%.%'"""
 POKESTOP_UPDATE_QUERY = """UPDATE {db_name}.{db_pokestop} set {db_pokestop_name}= %s, {db_pokestop_image} = %s WHERE {db_pokestop_id} = %s"""
 
-PORTAL_UPDATE_QUERY = """INSERT INTO {db_ingress}.ingress_portals(external_id, name, url, lat, lon, updated, imported) VALUES(%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE updated =%s, name=%s, url=%s, lat=%s, lon=%s"""
+PORTAL_UPDATE_QUERY = """INSERT INTO {db_ingress}.ingress_portals(external_id, name, url, lat, lon, updated, imported) VALUES(%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE updated=%s, name=%s, url=%s, lat=%s, lon=%s"""
 
 def create_config(config_path):
     """ Parse config. """
@@ -202,8 +202,10 @@ if __name__ == "__main__":
         for idx, val in enumerate(all_portals_id):
             lat = (all_portal_details[idx][2])/1e6
             lon = (all_portal_details[idx][3])/1e6
+            p_name = all_portal_details[idx][portal_name]
+            p_url = all_portal_details[idx][portal_url]
             updated_ts = datetime.datetime.now().strftime("%s")
-            insert_portal_args = (val,  all_portal_details[idx][portal_name],  all_portal_details[idx][portal_url], lat, lon, updated_ts, updated_ts, updated_ts, all_portal_details[idx][portal_name],  all_portal_details[idx][portal_url], lat, lon)
+            insert_portal_args = (val,  p_name,  p_url, lat, lon, updated_ts, updated_ts, updated_ts, p_name,  p_url, lat, lon)
             try:
                 mycursor_ingres.execute(portal_update_query, insert_portal_args)
                 print("~"*50)
@@ -212,7 +214,7 @@ if __name__ == "__main__":
             except Exception as e:
                 print(e)
                 print("#"*50)
-                print('could not put in db {0} ').format(val)
+                print('could not put in db {0} {1} ').format(val, all_portal_details[idx][portal_name])
                 print("#"*50)
 
     if args.all_poi:
